29d6fd6b0153183106ef5757dc5dd9da1eddeaa4
Muhammad Naufal. N <146636243+7765117706576@users.noreply.github.com>
2024-06-27 22:12:20 +0800

    Create Days068_Program_Huruf_Random.java

---
+++ Days068_Program_Huruf_Random.java
@@ -0,0 +1,20 @@
+package OHY;
+
+import java.util.*;
+
+public class Day_060_Program_huruf_Random {
+public static char getRandomChar(String teks) {
+        if (teks == null || teks.isEmpty()) {
+            throw new IllegalArgumentException("Teks tidak boleh kosong/null");
+        }
+        Random rand = new Random();
+        int indeksAcak = rand.nextInt(teks.length());
+        return teks.charAt(indeksAcak);
+    }
+    public static void main(String[] args) {
+        String teks = "Aku suka belajar koding";
+        char hurufAcak = getRandomChar(teks);
+        System.out.println("Huruf acak dari teks: " + hurufAcak);
+    }
+    
+}
