e8ee6af37cbf24b259ef4fa6d4e983916e9439d6
Muhammad Naufal. N <146636243+7765117706576@users.noreply.github.com>
2024-06-30 22:34:15 +0800

    Days071_Program_Konversi_Char_To_String.java

---
+++ Days071_Program_Konversi_Char_To_String.java
@@ -0,0 +1,14 @@
+package OHY;
+
+public class Day_063_Program {   
+public static void mymethod(){
+    char data[] = {'N','A','U','F','A','L'};
+    int range_Indeks = data.length;
+    String konversiData = new String(data);
+    System.out.println("String : "+konversiData);
+    System.out.println("panjang length : "+range_Indeks);
+} 
+    public static void main(String[] args) {
+        mymethod();
+    }
+}
